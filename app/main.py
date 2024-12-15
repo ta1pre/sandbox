0fcff5f2fb30c5ab373d0524f631272e10a5dce1
ta1pre <taichiumeki@gmail.com>
2024-12-15 23:19:20 +0900

    くじらさん

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
+# ファイル名: app/main.py
+
 from fastapi import FastAPI
 import os
-from mangum import Mangum  # Mangumをインポート
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     # 環境変数から設定を読み込む
-    message = os.getenv("MESSAGE", "Hello, World!")
+    message = os.getenv("MESSAGE", "こんにちわ、くじらさん")
     return {"message": message}
-
-# Lambdaエントリポイント
-handler = Mangum(app)
